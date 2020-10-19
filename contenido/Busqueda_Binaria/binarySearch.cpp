b52d800154b38f26e02149d1bd62445548491f03
paullandaeta <paulwilkerlf@gmail.com>
2020-10-19 07:52:50 -0400

    binary search

---
+++ contenido/Busqueda_Binaria/binarySearch.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h> 
+#define input freopen("in.txt", "r", stdin)
+#define output freopen("out.txt", "w", stdout)
+using namespace std;
+
+int main() {
+    input;
+   
+    return 0;
+}
