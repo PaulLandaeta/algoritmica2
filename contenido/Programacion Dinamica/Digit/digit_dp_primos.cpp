a0e174c4d1294492d3704cd8d69129a357f0f1ef
Paul Landaeta <paul.landeata@coxautoinc.com>
2021-08-18 18:44:15 -0400

    fixing

--- contenido/Programacion Dinamica/Digit/digit_dp_primos.cpp
+++ contenido/Programacion Dinamica/Digit/digit_dp_primos.cpp
@@ -5,13 +5,27 @@ using namespace std;
 
 // el numero maximo sera 10^18
 // 1000000000000000000
-string number = "307";
+string number;
 int dp[20][2][20];
 
-// modificar el 3er parametro de acuerdo al problema
-// 0 true 0
-// 0 ....... number
-int solve_dp(int pos, int mayor,int pares) {
+/* 
+* Crear una funcion para verificar si un numero es primo
+*
+*/ 
+bool isPrime(int n) {   
+    if (n == 2) return true;
+    if (n < 2 || n % 2 == 0) return false;
+    for (int i = 3; i * i <= n; i += 2)
+        if (n % i == 0) return false;
+    return true;
+}
+
+
+/* 
+* Digit DP para contar los numeros 
+* donde la suma de los digitos sea un numero primo 
+*/ 
+int solve_dp(int pos, int mayor,int total) {
     if(pos > number.size()) { // cuando la posicion exceda al numero dado
         return 0;
     }
@@ -24,19 +38,18 @@ int solve_dp(int pos, int mayor,int pares) {
             return 0;
         }
     }
-    dp[pos][mayor][pares] = 0;
+    dp[pos][mayor][total] = 0;
     if(dp[pos][mayor][pares] == -1) { // Pregunto si no lo he calculado 
         int tope = 9;
         if(mayor == true) {  // el numero que voy a crear puede llegar a ser mayor
             tope = number[pos]-'0';  // solo podemos usar los numeros de 0 al tope -- '3'-'0' =  51 - 48 = 3 
         }
-        dp[pos][mayor][pares] = 0;
         for(int digito = 0; digito <= tope; digito++) {
             if(digito == tope ) {
-                dp[pos][mayor][pares] += solve_dp(pos+1, true, pares + digito%2 == 0);
+                dp[pos][mayor][pares] += solve_dp(pos+1, true, digito+total);
             }
             else { // 0 1 2 
-                dp[pos][mayor][pares] += solve_dp(pos+1, false, pares + digito%2 == 0);
+                dp[pos][mayor][pares] += solve_dp(pos+1, false, digito+total);
             }
         }
     }
