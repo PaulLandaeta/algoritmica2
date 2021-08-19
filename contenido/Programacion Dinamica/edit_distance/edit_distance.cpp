c4d652037c91d1faaaa7ce334ac9a00deaa92450
Paul Landaeta <paul.landeata@coxautoinc.com>
2021-08-19 18:25:06 -0400

    edit distance td

---
+++ contenido/Programacion Dinamica/edit_distance/edit_distance.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h> 
+#define input freopen("in.txt", "r", stdin)
+#define output freopen("out.txt", "w", stdout)
+using namespace std;
+string wordA,wordB;
+
+int dp[10000][10000];
+int solve(int posA, int posB) {
+    if(posA == 0){
+        return posB;
+    } 
+    if(posB == 0){ 
+        return posA;
+    }
+    if(dp[posA][posB] == -1){
+        int minimoEditDistance = 100000000;
+    
+        // editar el caracter
+        int sonDiferentes  = wordA[posA] != wordB[posB]; // True o False -> 1 o 0
+        minimoEditDistance = min(minimoEditDistance,solve(posA-1, posB-1)+sonDiferentes);
+        // eliminar el caracter
+        minimoEditDistance = min(minimoEditDistance,solve(posA-1, posB)+1);
+        // adicionar el caracter
+        minimoEditDistance = min(minimoEditDistance,solve(posA, posB-1)+1);
+
+        dp[posA][posB] = minimoEditDistance;
+    }
+    return dp[posA][posB];
+}
+    
+    
+
+int main()
+{
+
+    wordA = "ACAAAAAAAAADASDASDAS";
+    wordB = "ACASDASFASFADFASDASDASD";
+    memset(dp,-1,sizeof(dp));
+    cout << solve(wordA.length()-1, wordB.length()-1) << endl; 
+
+    return 0;
+}
