6bc37cd3323a59a2e39879e5bdb3609a0fbd449b
Paul Landaeta <paul.landeata@coxautoinc.com>
2021-08-31 08:15:28 -0400

    dinic

---
+++ contenido/Teoria de Grafos/Dinic/dinic.cpp
@@ -0,0 +1,131 @@
+// Disclaimer: This code is a hybrid between old CP1-2-3 implementation of
+// Edmonds Karp's algorithm -- re-written in OOP fashion and the fast
+// Dinic's algorithm implementation by
+// https://github.com/jaehyunp/stanfordacm/blob/master/code/Dinic.cc
+// This code is written in modern C++17 standard
+
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef tuple<int, ll, ll> edge;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+
+const ll INF = 1e18;                             // large enough
+
+class max_flow {
+private:
+  int V;
+  vector<edge> EL;
+  vector<vi> AL;
+  vi d, last;
+  vector<ii> p;
+
+  bool BFS(int s, int t) {                       // find augmenting path
+    d.assign(V, -1); d[s] = 0;
+    queue<int> q({s});
+    p.assign(V, {-1, -1});                       // record BFS sp tree
+    while (!q.empty()) {
+      int u = q.front(); q.pop();
+      if (u == t) break;                         // stop as sink t reached
+      for (auto &idx : AL[u]) {                  // explore neighbors of u
+        auto &[v, cap, flow] = EL[idx];          // stored in EL[idx]
+        if ((cap-flow > 0) && (d[v] == -1))      // positive residual edge
+          d[v] = d[u]+1, q.push(v), p[v] = {u, idx}; // 3 lines in one!
+      }
+    }
+    return d[t] != -1;                           // has an augmenting path
+  }
+
+  ll send_one_flow(int s, int t, ll f = INF) {   // send one flow from s->t
+    if (s == t) return f;                        // bottleneck edge f found
+    auto &[u, idx] = p[t];
+    auto &cap = get<1>(EL[idx]), &flow = get<2>(EL[idx]);
+    ll pushed = send_one_flow(s, u, min(f, cap-flow));
+    flow += pushed;
+    auto &rflow = get<2>(EL[idx^1]);             // back edge
+    rflow -= pushed;                             // back flow
+    return pushed;
+  }
+
+  ll DFS(int u, int t, ll f = INF) {             // traverse from s->t
+    if ((u == t) || (f == 0)) return f;
+    for (int &i = last[u]; i < (int)AL[u].size(); ++i) { // from last edge
+      auto &[v, cap, flow] = EL[AL[u][i]];
+      if (d[v] != d[u]+1) continue;              // not part of layer graph
+      if (ll pushed = DFS(v, t, min(f, cap-flow))) {
+        flow += pushed;
+        auto &rflow = get<2>(EL[AL[u][i]^1]);     // back edge
+        rflow -= pushed;
+        return pushed;
+      }
+    }
+    return 0;
+  }
+
+public:
+  max_flow(int initialV) : V(initialV) {
+    EL.clear();
+    AL.assign(V, vi());
+  }
+
+  // if you are adding a bidirectional edge u<->v with weight w into your
+  // flow graph, set directed = false (default value is directed = true)
+  void add_edge(int u, int v, ll w, bool directed = true) {
+    if (u == v) return;                          // safeguard: no self loop
+    EL.emplace_back(v, w, 0);                    // u->v, cap w, flow 0
+    AL[u].push_back(EL.size()-1);                // remember this index
+    EL.emplace_back(u, directed ? 0 : w, 0);     // back edge
+    AL[v].push_back(EL.size()-1);                // remember this index
+  }
+
+  ll edmonds_karp(int s, int t) {
+    ll mf = 0;                                   // mf stands for max_flow
+    while (BFS(s, t)) {                          // an O(V*E^2) algorithm
+      ll f = send_one_flow(s, t);                // find and send 1 flow f
+      if (f == 0) break;                         // if f == 0, stop
+      mf += f;                                   // if f > 0, add to mf
+    }
+    return mf;
+  }
+
+  ll dinic(int s, int t) {
+    ll mf = 0;                                   // mf stands for max_flow
+    while (BFS(s, t)) {                          // an O(V^2*E) algorithm
+      last.assign(V, 0);                         // important speedup
+      while (ll f = DFS(s, t))                   // exhaust blocking flow
+        mf += f;
+    }
+    return mf;
+  }
+};
+
+int main() {
+  /*
+  // Graph in Figure 8.11
+  4 0 3
+  2 1 8 2 8
+  2 2 1 3 8
+  1 3 8
+  0
+  // the max flow value of that graph should be 16
+  */
+
+  freopen("maxflow_in.txt", "r", stdin);
+
+  int V, s, t; scanf("%d %d %d", &V, &s, &t);
+  max_flow mf(V);
+  for (int u = 0; u < V; ++u) {
+    int k; scanf("%d", &k);
+    while (k--) {
+      int v, w; scanf("%d %d", &v, &w);
+      mf.add_edge(u, v, w);                      // default: directed edge
+    }
+  }
+
+  // printf("%lld\n", mf.edmonds_karp(s, t));
+  printf("%lld\n", mf.dinic(s, t));
+
+  return 0;
+}
--- contenido/Teoria de Grafos/Edmonds_Karp/edmonds_karp.cpp
+++ contenido/Teoria de Grafos/Edmonds_Karp/edmonds_karp.cpp
@@ -15,39 +15,54 @@ int maxFlow(int s,int t) {
     // s nodo inicial 
     // t nodo final 
     int maxFlow = 0;
-    while(true) {
-        //BFS 
-        memset(path, -1, sizeof(path));
+    while(true) { ///  nunca sabemos si no tenemos flujo
+        //BFS O(V+E)
+        memset(path, -1, sizeof(path)); // no los estamos visitando
         queue<int> Q;
         Q.push(s);
-        path[s] = s; 
+        path[s] = s;  // s- 
         while(!Q.empty() && path[t]==-1) {
             int currentNode = Q.front();
             Q.pop();
             for(int i = 0 ; i < MAX_N ; i++) {
-                if(path[i]==-1 && grafo[currentNode][i] > 0) {  // puedo visitar? 
+                if(grafo[currentNode][i] > 0 && path[i]==-1)  {  // puedo visitar? 
                     path[i] = currentNode;                       // guardar camino 
                     Q.push(i);                                  // agregar a la cola
                 }   
             }
         }
-        int minFlow = 1e9;
-        if(path[t]==-1) {
+       
+        if(path[t]==-1) { // si llego a t el path tiene que ser diferente a -1
             break;
         }
+        int minFlow = 1e9; // infinito
         for(int from = path[t],to = t; from!=to ; from = path[to = from]) {
-            minFlow = min(minFlow,grafo[from][to]);
+            minFlow = min(minFlow,grafo[from][to]); // grafo[from][to] capacidades de las aristas 
         }
         
-        // Sumar y restar ese minimo                                            // sumar todos os diferentes a -1 
+
+        // V - 1 //  
+        // Sumar al grafo residual y restar al grafo de capacidad ese minimo                                            // sumar todos os diferentes a -1 
         for(int from = path[t],to = t; from!=to ; from = path[to = from]) {
-            grafo[to][from] += minFlow;
-            grafo[from][to] -= minFlow;
+            grafo[from][to] -= minFlow; // grafo de capacidad
+            grafo[to][from] += minFlow; // grafo residual
         }
-        
+        // s = 0 , t = 6
+        // 0 -> 1 -> 3 -> 5 -> 6 
+        // path[] = {0,0,0,1,0,3,5,0,0,0}
+        //           0 1 2 3 4 5 6 7 8 9
+
+        // from = 0
+        // to = 0
+
+        //from = path[to = from]
+        //from = path[0] 
         maxFlow += minFlow;
-        
+        // 2V+E-1
     }
+    // flujos * 2V+E-1
+    // flujos *2V + flujos*E - flujos
+    // flujos*E E^2
     return maxFlow;
 }
 
@@ -55,12 +70,12 @@ int maxFlow(int s,int t) {
 
 int main() {
 input;
-int nodes, vertices, nroCases = 1; 
+int nodes, aristas, nroCases = 1; 
 while(cin>>nodes && nodes) {
-    int s,t,vertices; 
-    cin>>s>>t>>vertices;
+    int s,t;   // s = fuente o source t = terminal 
+    cin>>s>>t>>aristas;
     memset(grafo,0,sizeof(grafo));
-     for(int i=0;i<vertices;i++) {
+     for(int i=0;i<aristas;i++) {
         int from, to, capacity; 
         cin>>from>>to>>capacity;
         grafo[from][to] = capacity;          // camino capacidad
